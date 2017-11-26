e72be34e280956ace2120ee7d2c9185f6491a440
yitzhak Matias <matias_6@hotmail.com>
2017-11-26 02:01:32 -0400

    #5 Project's migrations were added to solution

--- BookAuthor/BookAuthor.DL/BookContext.cs
+++ BookAuthor/BookAuthor.DL/BookContext.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
-namespace BookCatalog.DL
+namespace BookAuthor.DL
 {
     public class BookContext : DbContext
     {
-        public BookContext() : base("BookAuthor")
+        public BookContext() : base("ApplicationDbContext")
         {
         }
 

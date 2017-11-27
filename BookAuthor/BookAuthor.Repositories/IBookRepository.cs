a565bbcc6f8e899c4489eeee3169205bfd92b31f
yitzhak Matias <matias_6@hotmail.com>
2017-11-26 21:13:20 -0400

    #7 the Repostory pattern was implemented

---
+++ BookAuthor/BookAuthor.Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAuthor.Repositories
+{
+    public interface IBookRepository
+    {
+
+    }
+}
